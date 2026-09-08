11a5bd1f98d5cc6d0654df800e5053ee1cddba33
Alae <aamimi454@gmail.com>
2026-09-08 12:00:06 +0100

    creating UI and starting js navigation side

--- app.js
+++ app.js
@@ -15,3 +15,28 @@ const btnViewHistory = document.getElementById("btn-view-history");
 const btnConfigBack = document.getElementById("btn-config-back");
 const btnResultsHome = document.getElementById("btn-results-home");
 const btnResultsAgain = document.getElementById("btn-results-again");
+const btnConfigStart = document.getElementById("btn-config-start");
+const btnHistoryBack = document.getElementById("btn-history-back");
+
+btnStart.addEventListener("click", (e) => {
+    showView("view-config");
+})
+btnViewHistory.addEventListener("click", (e) => {
+    showView("view-history");
+})
+btnConfigBack.addEventListener("click", (e) => {
+    showView("view-home");
+})
+btnResultsHome.addEventListener("click", (e) => {
+    showView("view-home");
+})
+btnResultsAgain.addEventListener("click", (e) => {
+    showView("view-config");
+})
+btnConfigStart.addEventListener("click", (e) => {
+    e.preventDefault();
+    showView("view-game");
+})
+btnHistoryBack.addEventListener("click", (e) => {
+    showView("view-home");
+});
